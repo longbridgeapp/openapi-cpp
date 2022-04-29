11453c0b9f99a7b8d7b1b9e5f352fd6ce5d878f8
David <wendajiang93@163.com>
2022-04-29 10:58:55 +0800

    feat: token -> access_token (#2)
    * feat: token rename access_token
    
    * chore: http demo use english comments

--- demo/src/http.cpp
+++ demo/src/http.cpp
@@ -3,19 +3,22 @@
 //
 #include "http.h"
 
+#include <chrono>
 #include <iostream>
+
 #include "const_value.h"
 #include "logger.h"
-#include <chrono>
 using namespace std::chrono;
 using namespace std;
 
 /**
- * 推荐的 Client 用法
- * 因为每次创建 Client 的成本比较高
- * 一旦创建可以对同一个 url 的 uri 重复请求
- * 每个 auth 实例对应一个 Client
- * 如果有新的 Auth （多账户） 才需要多个 Client 实例
+ * As HttpClient new cost is a little high,
+ * so we recommend use SingleTon Pattern wrap
+ * HttpClient, and you can use one HttpClient
+ * send request repeatedly.
+ *
+ * Usually only if you have multi account(every account has a token),
+ * multi HttpClient needed.
  */
 class SingleTon {
  public:
@@ -23,7 +26,7 @@ class SingleTon {
     try {
       static http::Client http_client{kBaseUrl, kLanguage, kAuth};
       return http_client;
-    } catch (std::exception &e) {
+    } catch (std::exception& e) {
       LOG_ERR("create http_client failed", e.what());
     }
   }
@@ -38,22 +41,17 @@ int main() {
   http_internal::CxxRequestOption test{.timeout = 15};
   http::Client& http_client = SingleTon::get_http_client();
   try {
-    nlohmann::json query;
-    query["symbol"] = "700.HK";
-    query["start_at"] = 1650410999;
-    query["end_at"] = 1650510999;
-    query["order_id"] = 701276261045858304;
     http_internal::CxxResponse response =
-        http_client.get("/v1/test", query);
+        http_client.get("/v1/trade/asset/account", nlohmann::json{});
     std::string j{response.data};
-    for (auto header: response.headers) {
+    for (auto header : response.headers) {
       cout << header.key.c_str() << ": " << header.value.c_str() << std::endl;
     }
     std::cout << j << std::endl;
     nlohmann::json j2 = nlohmann::json::parse(response.data);
 
     std::cout << j2.dump(4) << std::endl;
-  } catch (std::exception &e) {
+  } catch (std::exception& e) {
     printf("http request error %s", e.what());
   }
 
